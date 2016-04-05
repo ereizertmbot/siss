b6b94a46fdd9d91ce7584cd1769cb953d83bacf6
ereizertmbot <mohamadsabori55@gmail.com>
2016-04-05 22:34:55 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {95822747},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V3
@@ -270,8 +270,9 @@ function create_config( )
     Arman
     IM/-\N
     Creed Is Dead
+    sevenadmin
     
-    Powered By @Sbss_Team
+    Powered By @sevenchanell
     
     #Open Source
     https://github.com/SbssTeam/Sbss
